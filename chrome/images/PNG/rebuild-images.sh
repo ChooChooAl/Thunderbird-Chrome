6183c565074a1d35753c31c15474a58472f48bca
ChooChooAl <26556059+ChooChooAl@users.noreply.github.com>
2021-02-04 16:25:00 -0500

    delete images folder

--- chrome/images/PNG/rebuild-images.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-montage `ls [0-5][0-9]*.png |sort` -background transparent -tile 5x11 -geometry 256x256 ../images.png
-montage `ls [0-5][0-9]*.png |sort` -background white -tile 11x5 -geometry 128x128 ../../Screen-Shots/icons.png
